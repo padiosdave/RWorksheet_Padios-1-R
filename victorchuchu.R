18c1bf1b20548c0487836dea1f7de14824bc4c4f
Dave Padios <dpadios2@gmail.com>
2023-09-20 04:08:22 +0000

    1

---
+++ victorchuchu.R
@@ -0,0 +1,35 @@
+AgeVec <-c(34,28,22,36,27,18,52,39,42,29,35,31,27,22,37,34,19,20,57,46,25,17,37,42,53,41,51,35,24,33,41)
+
+length(AgeVec)
+
+#2Find the Reciprocal of the values for age
+
+reciprocalAge <-1 /AgeVec
+reciprocalAge
+
+#3 assign also new_age 
+
+new_AgeVec <-c(AgeVec,0,AgeVec)
+new_AgeVec
+
+#4 Sort the values for age
+
+sortedAgeVec <- sort(AgeVec)
+sortedAgeVec
+
+#5 fint the minimum and maximum value for age
+
+minimum_AgeVec <- min (AgeVec)
+minimum_AgeVec
+
+maximum_AgeVec <- max (AgeVec)
+maximum_AgeVec
+
+#6 set up a vector named data
+
+new_dataVic <-2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.5,2.3,2.5,2.3,2.4,2.7
+
+
+
+
+
